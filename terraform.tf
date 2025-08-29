5160be81d752a539c66cc36a904fe6aa4bc04a91
ahsanmahmood95 <ahsan@DESKTOP-5LOEJ52.localdomain>
2025-08-29 03:21:08 -0700

    update

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.10.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-alpha-state-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-alpha-state-table"
+    
+  }
 }
